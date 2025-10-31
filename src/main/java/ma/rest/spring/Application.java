1c72289cac4f497981ed5fe0ec83e8ba7de01dcc
merouanneee147 <mechachmarouane@gmail.com>
2025-10-31 23:48:19 +0100

    first commit

---
+++ src/main/java/ma/rest/spring/Application.java
@@ -0,0 +1,23 @@
+package ma.rest.spring;
+
+import ma.rest.spring.entities.Compte;
+import ma.rest.spring.entities.TypeCompte;
+import ma.rest.spring.repositories.CompteRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
